6b451e10444bb3db99ec73d9cb7b8c7228c74914
Michael Tanczos <mike@logic-gate.com>
2024-01-17 22:42:04 -0500

    feat: Add global view transitions to App component
    - Added a meta tag with htmx-config attribute to enable global view transitions in the App component.
    
    fix: Update NavMenu component to swap innerHTML
    
    - Updated the hx-swap attribute in the NavMenu component to swap innerHTML instead of using default behavior.
    
    refactor: Remove commented code from blazor.js
    
    - Removed commented code related to onprogress and readystatechange events from the blazor.js file.

--- BlazorHtmx/wwwroot/js/blazor.js
+++ BlazorHtmx/wwwroot/js/blazor.js
@@ -54,43 +54,16 @@
                     var element = evt.detail.elt;
                     if (evt.detail.requestConfig.target) {
                         element['__target'] = evt.detail.requestConfig.target;
-                        element = evt.detail.requestConfig.target;
+                        //element = evt.detail.requestConfig.target;
                     }
 
                     var xhr = evt.detail.xhr;
-
-                    var lastLength = 0;
-                    //xhr.addEventListener('onprogress', function (e) {
-                    //    var newText = e.currentTarget.response;
-                    //});
                     xhr.onprogress = function (e) {
-                        var newText = e.currentTarget.response;
+                        swap(element, e.currentTarget.response);
                     };
-                    xhr.addEventListener('readystatechange', function () {
-                        if (xhr.readyState === 2 || xhr.readyState === 3) {
-                            var newText = xhr.responseText; //.substring(lastLength);
-                            element['__streamedChars'] = lastLength;
-                            lastLength = xhr.responseText.length;
-                            element.innerHTML = newText;
-                            //xhr.onload();
-                        }
-                    });
+
                 }
                 return true;
-            },
-            transformResponse: function (text, _xhr, elt) {
-                var lastLength = elt['__streamedChars'];
-                var target = elt['__target'];
-                if (target) {
-                    lastLength = target['__streamedChars'];
-                }
-
-                if (lastLength) {
-                    //var newText = text.substring(lastLength);
-                    //return newText;
-                }
-
-                return text;
             }
         });
 
