2e52d2d338f63078b38ef877444b6b47c578afa2
Michael Tanczos <tanczosm@eastonsd.org>
2024-01-23 13:58:23 -0500

    Remove unnecessary HTML, CSS, and JavaScript files and components
    - Deleted App.razor, LayoutContainer.razor, TestLayoutShell.razor
    - Added Friend.razor
    - Modified Home.razor to remove layout inheritance and add conditional rendering of sidebar based on HttpContext

---
+++ BlazorHtmx/Services/HtmxService.cs
@@ -0,0 +1,18 @@
+﻿using Htmx;
+
+namespace BlazorHtmx.Services
+{
+    public class HtmxService
+    {
+        private readonly IHttpContextAccessor? _httpContextAccessor = null;
+        private readonly HttpContext? _httpContext = null;
+
+        public HttpContext HttpContext => _httpContext ?? _httpContextAccessor?.HttpContext!;
+
+        public HtmxService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+    }
+}
