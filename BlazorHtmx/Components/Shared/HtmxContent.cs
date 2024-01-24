510680c07243def845faf0cc5f194d8ef0f0fda7
Michael Tanczos <tanczosm@eastonsd.org>
2024-01-24 14:42:19 -0500

    feat: Add HtmxRequestView and HtmxSwapView components
    - Added the `HtmxRequestView` component to handle HTMX requests and render content based on the request type.
    - Added the `HtmxSwapView` component to swap content based on the provided ID.
    - Updated the `Home.razor` file to include the new components and render test content.
    
    This commit introduces new components for handling HTMX requests and swapping content dynamically.

---
+++ BlazorHtmx/Components/Shared/HtmxContent.cs
@@ -0,0 +1,42 @@
+﻿using System.Xml;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BlazorHtmx.Components.Shared
+{
+    public class HtmxContent : ComponentBase
+    {
+        private Dictionary<string, object> _attributes = new Dictionary<string, object>();
+
+        [CascadingParameter]
+        public HtmxRequestView? Parent { get; set; }
+
+        [Parameter] 
+        public string Id { get; set; } = default!;
+
+        [Parameter] 
+        public RenderFragment ChildContent { get; set; } = default!;
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.AddContent(0, ChildContent);
+        }
+
+        /// <summary>
+        /// Register content with parent component
+        /// </summary>
+        /// <exception cref="ArgumentNullException"></exception>
+        protected override void OnInitialized()
+        {
+            if (Parent is null)
+                throw new ArgumentNullException(nameof(Parent), "HtmxContent must be placed inside an HtmxRequestView");
+            
+            Parent.AddContent(this);
+        }
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+        }
+    }
+}
---
+++ BlazorHtmx/Components/Shared/HtmxRequestView.cs
@@ -0,0 +1,61 @@
+﻿using Htmx;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Web.HtmlRendering;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorHtmx.Components.Shared;
+
+public class HtmxRequestView : ComponentBase
+{
+    protected List<HtmxContent> Containers { get; } = new();
+
+    [CascadingParameter]
+    public HttpContext? Context { get; set; }
+
+    [Parameter] 
+    public RenderFragment PageContent { get; set; } = default!;
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.OpenComponent<CascadingValue<HtmxRequestView>>(0);
+        builder.AddComponentParameter(1, "Value", this);
+        builder.AddComponentParameter(2, "ChildContent", (RenderFragment)((builder2) =>
+        {
+            if (Context?.Request.IsHtmx() == true)
+            {
+                for (int i = 0; i < Containers.Count; i++)
+                {
+                    var container = Containers[i];
+
+                    if (i == 0)
+                    {
+                        builder2.AddContent(4, container);
+                    }
+                    else
+                    {
+                        builder2.OpenComponent<HtmxSwapView>(5);
+                        builder2.AddComponentParameter(6, "Id", container.Id);
+                        builder2.AddAttribute(7, "ChildContent", (RenderFragment)((builder3) => {
+                            builder3.AddContent(8, container);
+                        }));
+                        builder2.CloseComponent();
+                    }
+                }
+            }
+            else
+            {
+                builder2.AddContent(9, PageContent);
+            }
+        }));
+        builder.CloseComponent();
+
+    }
+
+    internal void AddContent(HtmxContent content)
+    {
+        Containers.Add(content);
+    }
+}
+
+
